62da37a60c0b50ad92bdce9fed86fb1d1b2e2de0
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-27 12:35:33 +0700

    Deklarasi awal class siswa

---
+++ pointerSatu.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+class mahasiswa{
+    public:
+    int nim;
+    void showNim(){
+        cout << "No Induk ="<<nim<<endl;
+    }
+};
+
+int main (){
+    mahasiswa mhs{1};
+    mhs.showNim();
+
+    mahasiswa refMhs = mhs;
+    refMhs.nim = 2;
+    mhs.showNim();
+
+    mahasiswa *pMhs = &mhs;
+    pMhs -> nim = 3;
+    pMhs -> showNim();
+    return 0;
+}
---
+++ tugas.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class BelahKetupat;
+class LayangLayang {
+private:
+    int d1, d2;
+    int s;
+//
+}
