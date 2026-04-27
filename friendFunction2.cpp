99ca63011df0b080703a71fbc1fc2d31826a6165
sulisamelia-sketch <sulisamelia256@gmail.com>
2026-04-27 12:53:45 +0700

    Membuat class pelajar

--- friendFunction2.cpp
+++ friendFunction2.cpp
@@ -8,3 +8,17 @@ class manusia
     void showNilaiPelajar(pelajar&x);
 };
 
+class pelajar
+{
+    private:
+    int nilai;
+
+    public:
+    int nilai;
+
+    public:
+
+pelajar() { nilai = 100;}
+friend void manusia::showNilaiPelajar(pelajar &x);
+};
+
